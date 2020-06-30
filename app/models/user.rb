7f067fe211e9829995bf6678728045bf079b8464
Megan_Comeau <mlcomeau99@gmail.com>
2020-06-30 12:12:28 -0700

    creates model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :reviews 
+    has_many :wines, through: :reviews 
 end
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,4 +1,6 @@
 class Wine < ApplicationRecord
   belongs_to :region
   belongs_to :varietal
+  has_many :reviews 
+  has_many :users, through: :reviews 
 end
