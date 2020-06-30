4038277c5df9c477f4d1e4b743b7eacf5df2b1c1
Megan_Comeau <mlcomeau99@gmail.com>
2020-06-30 15:44:15 -0700

    add login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     end 
 
     def create 
-        @user = User.new(user_params(:username, :password))
+        @user = User.(user_params(:username, :password))
         if @user.save
             session[:user_id] = @user.id
             redirect_to user_path(@user)
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     end
 
     def show 
-        @user = User.find_by(params[:id])
+        @user = User.find(params[:id])
     end 
     
 
